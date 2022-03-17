3019eaa71103ffbd79a497f1242669901cbc52eb
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-17 22:48:15 +0530

    feat: static data added to common file

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+  slides: {
+    sliderImages: [
+      `${process.env.PUBLIC_URL}/images/Hangout-rafiki.svg`,
+      `${process.env.PUBLIC_URL}/images/Workout-rafiki.svg`,
+      `${process.env.PUBLIC_URL}/images/Ebook-pana.svg`,
+    ],
+    inititalSlide: 0,
+  },
+  logoImage: {
+    logoPath: "/assets/logo/logo.svg",
+    logoDesc: "Logo for 3 on 3 store",
+  },
+};
+
+export { data };
