7d887021f2a84243fd8d59016efac4680ae0dc36
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-17 22:49:55 +0530

    reafctor: code refactoring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Home} from "./pages/home/home.js";
+import { Home } from "./pages/home/home.js";
 import "./styles/reset.css";
 import "./App.css";
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
