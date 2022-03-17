c342359daeed7465a3b0ffde7b2e79f27cb5287a
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-17 22:54:08 +0530

    reafctor: code refactoring

--- src/pages/home/header/header.js
+++ src/pages/home/header/header.js
@@ -1,11 +1,13 @@
-import {Navbar} from "../../../components/navbar/navbar.js"
+import { Navbar } from "../../../components/navbar/navbar.js";
+import { data } from "../../../data.js";
 
-function Header(){
-    return(
-        <header className="header">
-            <Navbar logoImage={"logo image object has to be passed here containing img path and img alt text"}/>
-        </header>
-    )
+function Header() {
+  const { logoImage } = data;
+  return (
+    <header className="header">
+      <Navbar logoImage={logoImage} />
+    </header>
+  );
 }
 
-export {Header};
+export { Header };
--- src/pages/home/main/main.js
+++ src/pages/home/main/main.js
@@ -1,46 +1,49 @@
 import { ImageSlider } from "../../../components/imageslider/imageslider.js";
+import { Link } from "react-router-dom";
+import { data } from "../../../data.js";
 
 function Main() {
+  const { sliderImages, inititalSlide } = data.slides;
   return (
     <main className="main">
-      <ImageSlider sources={"array of images src"} />
+      <ImageSlider sliderImages={sliderImages} inititalSlide={inititalSlide} />
 
       <div className="column-padding">
-      <blockquote>
-      <div className="content-grid">
-          <h3 className="text-xxl">3 on 3 Store</h3>
-          <div className="text-md">
-
+        <blockquote>
+          <div className="content-grid">
+            <h3 className="text-xxl">3 on 3 Store</h3>
+            <div className="text-md">
               <div>
-              <div>Three things:</div>
-              <div className="dflex">
-              <span>- Strength/Aesthetics/Fitness</span>
-              <span>- Knowledge/Wisdom/Intelligence</span>
-              <span>- Social Skills/Machiavellianism/Game</span>
-            </div>
+                <div>Three things:</div>
+                <div className="dflex">
+                  <span>- Strength/Aesthetics/Fitness</span>
+                  <span>- Knowledge/Wisdom/Intelligence</span>
+                  <span>- Social Skills/Machiavellianism/Game</span>
+                </div>
               </div>
 
-            <div>
-            <div> You want to work on all three of them.</div>
+              <div>
+                <div> You want to work on all three of them.</div>
 
-<div className="dflex">
-  <span>- You do not want to be 1/3.</span>
-  <span>- You do not want to be 2/3.</span>
-  <span>- You want to be 3/3.</span>
-</div>
+                <div className="dflex">
+                  <span>- You do not want to be 1/3.</span>
+                  <span>- You do not want to be 2/3.</span>
+                  <span>- You want to be 3/3.</span>
+                </div>
+              </div>
+
+              <div>
+                <div>Don't severely neglect one.</div>
+                <div>
+                  <strong>~Harsh Strongman</strong>
+                </div>
+              </div>
+              <Link className="btn btn-secondary" to="/products">
+                Buy Now
+              </Link>
             </div>
-
-          <div>
-          <div>Don't severely neglect one.</div>
-            <div><strong>~Harsh Strongman</strong></div>
           </div>
-            {/* a href tag needed to be fixed with routes and links */}
-            <a className="btn btn-secondary" href="pages/products.html">
-              Buy Now
-            </a>
-          </div>
-        </div>
-      </blockquote>
+        </blockquote>
       </div>
     </main>
   );
