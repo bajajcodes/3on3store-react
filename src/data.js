8a5a35d96916e889dc5ced1301ae1bcb33fdcae1
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-18 18:12:31 +0530

    feat: static data for auth

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = {
+    logoImage: {
+      logoPath: "/assets/logo/logo.svg",
+      logoDesc: "Logo for 3 on 3 store",
+    },
+    ecommerceName : "3 on 3 Store",
+    headerInfo : {
+        login:{
+            authTo : "signup",
+            authToName : "Signup",
+            authMessage : "Welcome back.",
+        },
+        signup:{
+            authTo : "login",
+            authToName : "Login",
+            authMessage : "Join over 100,000 people who learned to play the game, read actionable items and get fitter on 3 on Store.",
+        }
+    },
+    asideImages:{
+        asideLoginImage:{
+            imagePath: "/images/login-cuate_black.png",
+            imageDesc: "Login visualizer",
+        },
+        asideSignupImage:{
+            imagePath: "/images/Signup-cuate_black.png",
+            imageDesc: "Signup visualizer",
+        }
+    }
+  };
+  
+  export { data };
