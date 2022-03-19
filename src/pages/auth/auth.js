4ddd7f36c726cd5f4be9af5ebca80b538e415511
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-19 18:23:17 +0530

    feat: absolute import from baseurl

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -2,8 +2,9 @@ import "./auth.css";
 import { AuthHeader } from "./header/header.js";
 import { AuthAsideImage } from "./aside/aside.js";
 import {AuthLogin, AuthSignup} from "./main/main.js";
-import {data} from "../../data";
+import {data} from "data";
 
+// TODO: make these components import dynamic
 function Login() {
   return (
     <div className="auth">
