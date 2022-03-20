b9f04dbd8707bef9b60c3a9fd01a019ea39e1742
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-20 20:46:41 +0530

    refactor: Card and StarRating components import added

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export {Footer} from "./footer/footer";
-export {Header} from "./header/header";
-export {ImageSlider} from "./imageslider/imageslider";
-export {Navbar} from "./navbar/navbar";
+export { Footer } from "./footer/footer";
+export { Header } from "./header/header";
+export { Navbar } from "./navbar/navbar";
+export { StarRating } from "./StarRating/starRating";
+export { Card } from "./card/card";
