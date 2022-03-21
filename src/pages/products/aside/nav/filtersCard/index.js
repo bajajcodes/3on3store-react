c134b2170d2c03d06d8774eadff1e9270394915a
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-21 15:45:22 +0530

    refactor: files deleted

--- src/pages/products/aside/nav/filtersCard/index.js
+++
@@ -1,4 +0,0 @@
-export { FilterHeader } from "./filterCardHeader";
-export { FilterCategory } from "./filterCardCategory";
-export { FilterRating } from "./filterCardRating";
-export { FilterSortBy } from "./filterCardSortBy";
