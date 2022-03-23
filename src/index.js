73f8e43c559014b9ac813753231866890f520710
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-24 01:38:41 +0530

    feat: context provided for products page context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { makeServer } from "server";
 import { RouteSwitch } from "RouteSwitch";
+import { ProductProvider } from "context";
 
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <ProductProvider>
+      <RouteSwitch />
+    </ProductProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
