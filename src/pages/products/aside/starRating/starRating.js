5fdda61d22c7b215fbbc0800ca47419d6419e3c3
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-24 01:37:40 +0530

    feat: converted into local products page component

---
+++ src/pages/products/aside/starRating/starRating.js
@@ -0,0 +1,29 @@
+import { Star } from "components";
+import { useProducts } from "context";
+
+function StarRating({ starColor }) {
+  const {productsState, productsDispatch} = useProducts();
+  const { rating } = productsState;
+
+  function hoverOver(event) {
+    if (event && event.target && event.target.getAttribute("data-star-id")) {
+      let val = parseInt(event.target.getAttribute("data-star-id"), 10);
+      productsDispatch({ type: "RATING", rating: val });
+    }
+  }
+
+  return (
+    <div onMouseOut={() => hoverOver(null)} onMouseOver={hoverOver}>
+      {Array.from({ length: 5 }, (v, i) => (
+        <Star
+          starId={i + 1}
+          key={`star_${i + 1}`}
+          marked={rating ? rating >= i + 1 : 0}
+          starColor={starColor}
+        />
+      ))}
+    </div>
+  );
+}
+
+export { StarRating };
