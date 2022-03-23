163f847f5867bbcfd184079fc1023e4f25151d2a
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-23 09:42:18 +0530

    feat: products and categories added to db

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    firstName: "shubham",
+    lastName: "bajaj",
+    email: "shubham@bajaj.com",
+    password: "shubham.bajaj",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
