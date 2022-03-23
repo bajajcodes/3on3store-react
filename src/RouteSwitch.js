c7f69753e6566768780b32118cff57716fea15e4
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-24 01:35:18 +0530

    refactor: product page nested routes and mockman route added

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Login, Signup, Products, Wishlist, Cart} from "pages";
+import Mockman from "mockman-js";
 
 function RouteSwitch() {
   return (
@@ -8,10 +9,13 @@ function RouteSwitch() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="/products" element={<Products />} >
+          <Route path=":category" element={<Products />}/>
+        </Route>
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Home />} />
+        <Route path="/mock" element={<Mockman />} />
       </Routes>
     </Router>
   );
