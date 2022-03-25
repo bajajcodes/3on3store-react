046511f346533b83c99929317b6b29cd5b6e0821
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-26 00:33:20 +0530

    feat: styles control using cart context

--- src/pages/cart/main/cardContainer.js
+++ src/pages/cart/main/cardContainer.js
@@ -1,13 +1,17 @@
 import { Card } from "components";
-import { products } from "dump";
+import { useCartContext } from "context";
 
 function CardContainer() {
+  const { cartState } = useCartContext();
+  const { cart, getCartSummary } = cartState;
+  const { numberOfProductsInCart } = getCartSummary(cart);
   return (
-    <div className="cards-container">
-      {products &&
-        products.map((product, index) => (
-          <Card product={product} productInCart={true} key={index} />
-        ))}
+    <div
+      className="cards-container"
+      style={{ overflowY: cart.length > 1 ? "scroll" : "hidden" }}
+    >
+      {cart &&
+        cart.map((product, index) => <Card product={product} key={index} />)}
     </div>
   );
 }
