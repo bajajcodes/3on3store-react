c6ad3178f2cb22ba1c4ad8e3ef446c620157a30a
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-26 00:33:32 +0530

    feat: styles control using cart context

--- src/pages/cart/main/main.js
+++ src/pages/cart/main/main.js
@@ -1,10 +1,17 @@
 import { CardContainer } from "./cardContainer";
+import { useCartContext } from "context";
 
 function Main() {
+  const { cartState } = useCartContext();
+  const { cart, getCartSummary } = cartState;
+  const { numberOfProductsInCart } = getCartSummary(cart);
   return (
     <main className="main">
-      <h2 className="text-xl content-title">My Cart</h2>
-
+      <h2 className="text-xl content-title">
+        {numberOfProductsInCart
+          ? `My Cart (${numberOfProductsInCart})`
+          : `Your Cart is empty 😦`}
+      </h2>
       <CardContainer />
     </main>
   );
