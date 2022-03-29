dd978d09a3cf7d15a6b8845ca3c93c276a2fe7d8
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-29 11:26:12 +0530

    feat: basic auth integration done

--- src/pages/wishlist/wishlist.js
+++ src/pages/wishlist/wishlist.js
@@ -2,8 +2,22 @@ import { Header, Footer } from "components/index";
 import { Main } from "./main/main";
 import "pages/products/products.css";
 import "./wishlist.css";
+import { useAuthContext } from "context";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function Wishlist() {
+  const {
+    authState: { loginStatus },
+  } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loginStatus === false) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div className="wishlist products-wrapper">
       <Header />
